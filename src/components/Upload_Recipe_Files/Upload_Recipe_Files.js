e2605f24b91c92e33fb31adfb1904402bf49e25d
Mansi Singh <mansisingh@Mansis-MacBook-Air.local>
2021-07-23 20:26:05 -0500

    Upload file code

--- src/components/Upload_Recipe_Files/Upload_Recipe_Files.js
+++ src/components/Upload_Recipe_Files/Upload_Recipe_Files.js
@@ -3,20 +3,53 @@ import PropTypes from 'prop-types';
 import './Upload_Recipe_Files.css';
 import Header from '../Header/Header';
 import SideBar from '../SideBar/SideBar'
-
-// const Upload_Recipe_Files = () => (
-//   <div className={styles.Upload_Recipe_Files}>
-//     Upload_Recipe_Files Component
-//   </div>
-// );
+import {useState} from 'react';
+import axios from 'axios';
 
 function Upload_Recipe_Files() {
+    const [uploadFile, setUploadFile] = React.useState();
+    const [superHero, setSuperHero] = React.useState();
+    
+    const submitForm = (event) => {
+      event.preventDefault();
+  
+      const dataArray = new FormData();
+      dataArray.append("superHeroName", superHero);
+      dataArray.append("uploadFile", uploadFile);
+
+    axios
+    .post("url here", dataArray, { // paste url here!
+      headers: {
+        "Content-Type": "multipart/form-data"
+      }
+    })
+    .then((response) => {
+        alert(response);
+    })
+    .catch((error) => {
+      alert(error)
+    });
+};
+
 return(
   <body>
       <Header />
       <SideBar />
-        <div className="uploadFile-screen-container">
-           <div className="uploadFile-screen-content">
+      <div className="uploadFile-screen-container">
+        <div className="uploadFile-screen-content">
+      <div>
+      <form onSubmit={submitForm}>
+        <input
+          type="text"
+          onChange={(e) => setSuperHero(e.target.value)}
+          placeholder={"File name"}
+        />
+        <br />
+        <input type="file" onChange={(e) => setUploadFile(e.target.files)} />
+        <br />
+        <input type="submit" />
+      </form>
+            </div>
            </div>
         </div>
   </body>
